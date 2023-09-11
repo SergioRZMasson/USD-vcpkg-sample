d1109a6c3eb3e77980cbd4a7b7498e5e799d26ab
Sergio Ricardo Zerbetto Masson <sergioze@microsoft.com>
2023-09-11 15:36:31 -0300

    Fixed plugins loading

--- main.cpp
+++ main.cpp
@@ -1,26 +1,33 @@
-#include "pxr/usd/usd/notice.h"
-#include "pxr/usd/usd/stage.h"
-#include "pxr/usd/usd/prim.h"
+#include <pxr/usd/usd/stage.h>
+#include <pxr/usd/usdGeom/xform.h>
+#include <pxr/usd/usdGeom/sphere.h>
 #include <pxr/usd/ar/defaultResolver.h>
+#include <pxr/base/plug/plugin.h>
+#include <pxr/base/plug/registry.h>
+#include <filesystem>
+
+#include <winrt/base.h>
+
+namespace fs = std::filesystem;
+
+std::filesystem::path GetModulePath()
+{
+    WCHAR modulePath[4096];
+    DWORD result = GetModuleFileNameW(nullptr, modulePath, ARRAYSIZE(modulePath));
+    winrt::check_bool(result != 0 && result != std::size(modulePath));
+    return std::filesystem::path{ modulePath }.parent_path();
+}
 
 int main()
 {
-    const char* fileName = ".\\Kitchen_set.usd";
-    pxr::ArDefaultResolver::SetDefaultSearchPath({"./"});
-    bool bIsSupported = PXR_NS::UsdStage::IsSupportedFile(fileName);
-    if (bIsSupported)
-    {
-        printf("it is supported!\n");
-        PXR_NS::UsdStageRefPtr loadedStage = PXR_NS::UsdStage::Open(fileName);
-        if (loadedStage)
-        {
-            PXR_NS::UsdPrim pseudoRoot = loadedStage->GetPseudoRoot();
-            printf("Pseudo root path: %s\n", pseudoRoot.GetPath().GetString().c_str());
-            for (PXR_NS::UsdPrim const& c : pseudoRoot.GetChildren())
-            {
-                printf("\tChild path: %s\n", c.GetPath().GetString().c_str());
-            }
-        }
-    }
-    return 0;
+    auto currentPath = GetModulePath().string();
+    std::replace(currentPath.begin(), currentPath.end(), '\\', '/');
+    currentPath += "/usd/";
+    auto plugins = pxr::PlugRegistry::GetInstance().RegisterPlugins(currentPath);
+
+    auto stage = pxr::UsdStage::CreateNew("HelloWorld.usda");
+    auto xform = pxr::UsdGeomXform::Define(stage, pxr::SdfPath("/hello"));
+    auto sphere = pxr::UsdGeomSphere::Define(stage, pxr::SdfPath("/hello/world"));
+    stage->GetRootLayer()->Save();
 }
+
